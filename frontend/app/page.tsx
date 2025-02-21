8b6c1aebe696bc5fa0f83ee3bcd623cdc0576681
Logan <loganclingner@gmail.com>
2025-02-21 13:07:51 -0600

    moved homescreen file back into app folder

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -48,7 +48,7 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Project Repository Test
+          Project Repository
         </a>
         <a
           className="flex items-center gap-2 hover:underline hover:underline-offset-4"
