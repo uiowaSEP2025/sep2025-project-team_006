496eee75c176d47e57ad4cd21a60edc46541febf
Logan <loganclingner@gmail.com>
2025-02-21 18:19:00 -0600

    Testing out ways to push to github pages

---
+++ frontend/next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    output: "export",
+    distDir: "out",
+    basePath: "/sep2025-project-team_006",
+    images: {
+        unoptimized: true,
+    },
+};
+module.exports = nextConfig;
