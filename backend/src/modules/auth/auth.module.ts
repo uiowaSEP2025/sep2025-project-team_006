063bea000337a7d5e28e8146ed2355dd4e82cc98
mechabubba <stevenvanniisprettycool@gmail.com>
2025-02-24 12:00:24 -0600

    create a basic login setup
    this just implements two basic routes, `POST api/auth/login` and `POST api/auth/register`

--- backend/src/modules/auth/auth.module.ts
+++ backend/src/modules/auth/auth.module.ts
@@ -4,11 +4,12 @@ import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entity/user.entity';
+import { Session } from 'src/entity/session.entity';
 
 ConfigModule.forRoot();
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User, Session])],
   controllers: [AuthController],
   providers: [AuthService],
 })
--- backend/src/modules/auth/auth.service.ts
+++ backend/src/modules/auth/auth.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { User } from 'src/entity/user.entity';
+import { Session } from 'src/entity/session.entity';
 import { Repository } from 'typeorm';
+import * as crypto from 'node:crypto';
 
 /**
  * Job: Handles authentication; session management, logins, etc.
@@ -10,16 +12,38 @@ import { Repository } from 'typeorm';
 export class AuthService {
   constructor(
     @InjectRepository(User)
+    @InjectRepository(Session)
     private readonly userRepository: Repository<User>,
+    private readonly sessionRepository: Repository<Session>
   ) {}
 
   async register(email: string, password: string) {
-    // stub
-    return;
+    if(await this.userRepository.findOne({ where: { email: email } })) {
+      return false; // exists.
+    };
+    let record = this.userRepository.create({
+      email: email,
+      passwordDigest: password, // VERY SUPER TEMPORARY. didn't want to figure out password digestion. shouldn't be super hard though...
+    });
+    this.userRepository.save(record);
+    return true;
   }
 
   async login(email: string, password: string) {
-    // stub
-    return;
+    let userRecord = await this.userRepository.findOne({ where: { email: email } });
+    if(!userRecord) {
+      return false; // doesn't exist
+    };
+    if (password !== userRecord.passwordDigest) {
+      return false;
+    }
+
+    let token = crypto.randomBytes(32).toString("hex");
+    let session = this.sessionRepository.create({
+      userID: userRecord.id,
+      token: token,
+    })
+    this.sessionRepository.save(session);
+    return token;
   }
 }
