b1043defeafc25166bc5074e94aeb99c0e11e065
mechabubba <stevenvanniisprettycool@gmail.com>
2025-02-27 14:09:48 -0600

    actually add the auth module to the app modules imports

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TestModule } from './modules/test/test.module';
 import { DatabaseModule } from './database/database.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     TestModule,
+    AuthModule
   ],
 })
 export class AppModule {}
