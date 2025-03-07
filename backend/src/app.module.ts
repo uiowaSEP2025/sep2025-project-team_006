e5eb71117d3ccd6c8c61f50bd916ee8c0a578768
Brandon Cano <brandoncano01@gmail.com>
2025-03-07 17:11:39 -0600

    Added to the main module to be used

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,12 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { TestModule } from './modules/test/test.module';
 import { DatabaseModule } from './database/database.module';
 import { FacultyMetricsModule } from './modules/faculty-metrics/faculty-metrics.module';
+import { StudentsModule } from './modules/students/students.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
     FacultyMetricsModule,
+    StudentsModule,
     TestModule,
   ],
 })
