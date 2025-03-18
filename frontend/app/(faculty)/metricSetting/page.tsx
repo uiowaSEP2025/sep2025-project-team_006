99063e5bab357c6d9ee1d9a48495cd925a1fe363
joBrady <brady.joe63@gmail.com>
2025-03-18 17:42:01 -0500

    Added fetchMetrics into MetricSetting Page to make an API call to the backend

--- frontend/app/(faculty)/metricSetting/page.tsx
+++ frontend/app/(faculty)/metricSetting/page.tsx
@@ -14,5 +14,31 @@ interface Metric {
 }
 
 export default function Home() {
+ const router = useRouter();
+ const webService = new WebService();
+ const [metrics, setMetrics] = useState<Metric[]>([]);
 
+ useEffect(()=> {
+    const fetchMetrics = async () => {
+        try{
+            const response = await apiGET(webService.FACULTY_METRIC_GET)
+            if (response.success) {
+                const fetchedMetrics: Metric[] = response.payload.map(
+                    (metric: any) => ({
+                        id: metric.id,
+                        name: metric.name,
+                        description: metric.description,
+                        weight: metric.weight,
+                    })
+                );
+                setMetrics(fetchedMetrics);
+            } else{
+                console.log("GET error: ", response.error);
+            }
+        } catch (error){
+            console.log("An unexpected error occured: ", error)
+        }
+    }
+    fetchMetrics()
+ }, [webService.FACULTY_METRIC_GET]);
 }
