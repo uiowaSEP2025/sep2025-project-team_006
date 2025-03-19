c3cb311563df5bcdd5de4e68307778911fe1b6a0
joBrady <brady.joe63@gmail.com>
2025-03-18 19:56:18 -0500

    Modified WebServices to reflect changes provided by Brandon on Teams

--- frontend/api/WebService.ts
+++ frontend/api/WebService.ts
@@ -11,8 +11,8 @@ export default class WebService {
     TEST_PUT = `${this.serverUrl}/api/test/:id`;
     TEST_POST = `${this.serverUrl}/api/test`;
     // API calls
-    FACULTY_METRIC_ID = `${this.serverUrl}/api/faculty/metrics/:id`; // PUT, POST, DELETE
-    FACULTY_METRIC_GET = `${this.serverUrl}/api/faculty/metrics`; // GET
+    FACULTY_METRIC_ID = `${this.serverUrl}/api/faculty/metrics/:id`; // GET, PUT, DELETE
+    FACULTY_METRIC_POST = `${this.serverUrl}/api/faculty/metrics`; // POST
     FACULTY_METRIC_DEFAULTS = `${this.serverUrl}/api/faculty/metrics/default` // GET
     STUDENTS_APPLICANT_LIST = `${this.serverUrl}/api/students/applicants`; // GET
     STUDENTS_APPLICANT_INFO = `${this.serverUrl}/api/students/:id`; // GET
