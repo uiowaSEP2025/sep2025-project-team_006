736b4c2540fc2fa94797588deacaadaa09299017
joBrady <brady.joe63@gmail.com>
2025-03-25 15:35:22 -0500

    Removed old components from login page

--- frontend/app/(faculty)/faculty/page.tsx
+++ frontend/app/(faculty)/faculty/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Link from 'next/link';
 import { LoginForm } from "@/components/Login"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
@@ -15,27 +16,15 @@ export default function Home() {
                   priority
          />
         <LoginForm />
-        <form action="." className="max-w-xs mx auto">
-          <div className="mb-3">
-              <input name="username"autoComplete="off" type ="text" placeholder="Username" className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"></input>
-            </div>
-            <div className="mb-3">
-              <input name="password"autoComplete="off" type ="text" placeholder="Password" className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"></input>
-            </div>
-            
-            <button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
-              Login</button>
-        </form>
-
+        
         <div className="flex gap-4 items-center flex-col sm:flex-row">
-
+          <Button asChild>
           <Link
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
             href="/facultyHome"
-            rel="noopener noreferrer"
           >
             Move to Next Page
           </Link>
+          </Button>
         </div>
       </main>
     </div>
