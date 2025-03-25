b006f8cd3b8c8719f64f715169db710996ad3778
joBrady <brady.joe63@gmail.com>
2025-03-25 18:07:27 -0500

    Finalized base look for student home page

--- frontend/app/(student)/studentHome/page.tsx
+++ frontend/app/(student)/studentHome/page.tsx
@@ -4,36 +4,50 @@ import  HomeDashboard from "@/components/HomeDashboard";
 
 export default function Home() {
 
-  const TestingJunk = [
-    {label: "RockOne", href: "/#"},
-    {label: "RockTwo", href: "/#"},
-    {label: "RockThree", href: "/#"},
+  const AdmissionsInformation = [
+    {label: "Admissions Profile", href: "/#", image: "/JPGOFIcon.png"},
+    {label: "Financial Aid", href: "/#", image: "/JPGOFIcon.png"},
+    {label: "Visit Campus", href: "/#", image: "/JPGOFIcon.png"},
+    {label: "Scholarship Portal", href: "/#", image: "/JPGOFIcon.png"},
+    {label: "Housing Information", href: "/#", image: "/JPGOFIcon.png"},
   ];
 
-  const learningItems = [
-    {label: "FireOne", href: "/#"},
-    {label: "FireTwo", href: "/#"},
-    {label: "FireThree", href: "/#"},
+  const StudentLearning = [
+    {label: "ICON", href: "/#", image: "/JPGOFIcon.png"},
+    {label: "SPOT", href: "/#", image: "/JPGOFIcon.png"},
+    {label: "Study Spaces", href: "/#", image: "/JPGOFIcon.png"},
+    {label: "Student Spaces", href: "/#", image: "/JPGOFIcon.png"},
+    {label: "Printing", href: "/#", image: "/JPGOFIcon.png"},
+    {label: "LinkedIn Learning", href: "/#", image: "/JPGOFIcon.png"},
+    {label: "Academic Support", href: "/#", image: "/JPGOFIcon.png"},
+    {label: "UI Libraries", href: "/#", image: "/JPGOFIcon.png"},
   ];
+
+  const StudentInformation = [
+    {label: "Academic Calendar", href: "/#", image: "/JPGOFIcon.png"},
+    {label: "Address Change", href: "/#", image: "/JPGOFIcon.png"},
+    {label: "Advising Appointment", href: "/#", image: "/JPGOFIcon.png"},
+    {label: "Admissions Profile", href: "/#", image: "/JPGOFIcon.png"},
+  ];
+
+  const StudentInvolvement = [
+    {label: "TestOne", href: "/#", image: "/JPGOFIcon.png"},
+    {label: "TestTwo", href: "/#", image: "/JPGOFIcon.png"},
+    {label: "TestThree", href: "/#", image: "/JPGOFIcon.png"},
+  ];
+
+
+
   
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-start justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1 className="text-4xl md:text-6xl font-extrabold mb-6 underline underline-offset-4 decoration-black"> STUDENT HOME PAGE </h1>
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <HomeDashboard title ="TestingJunk" items={TestingJunk}/>
-        <HomeDashboard title="TestingFunk" items={learningItems}/>
-
-        <div className="flex gap-12 items-center flex-col sm:flex-row">
-          <Link
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="/studentList">
-            Student List-View
-          </Link>
-          <Link
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="/metricSetting">
-            Settings
-          </Link>
-        </div>
+        <HomeDashboard title ="Admissions Information" items={AdmissionsInformation}/>
+        <HomeDashboard title="Student Learning" items={StudentLearning}/>
+        <HomeDashboard title="Student Information" items={StudentInformation}/>
+        <HomeDashboard title="Student Involvement and Support" items={StudentInvolvement}/>
+        
       </main>
     </div>
   );
