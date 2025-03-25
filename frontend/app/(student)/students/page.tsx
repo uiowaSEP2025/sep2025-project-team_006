ad7bf0566cc6db05b87f20361b271661ec0e76e2
joBrady <brady.joe63@gmail.com>
2025-03-25 16:14:34 -0500

    Updated StudentLogin to match facultyLogin styling

--- frontend/app/(student)/students/page.tsx
+++ frontend/app/(student)/students/page.tsx
@@ -1,49 +1,38 @@
 import Image from "next/image";
 import Link from 'next/link';
+import { LoginForm } from "@/components/Login"
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-         <Image
-                  className="light"
-                  src="./GAPpaint.png"
-                  alt="GAP logo"
-                  width={360}
-                  height={38}
+        <Image
+              className="light"
+              src="./GAPpaint.png"
+              alt="GAP logo"
+              width={360}
+              height={38}
                   priority
-                />
-
-        <p className="text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          Student Sign-In Page
-        </p>
-
-        <form action="." className="max-w-xs mx auto">
-          <div className="mb-3">
-              <input name="username"autoComplete="off" type ="text" placeholder="Username" className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"></input>
-            </div>
-            <div className="mb-3">
-              <input name="password"autoComplete="off" type ="text" placeholder="Password" className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"></input>
-            </div>
-            <button className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5">
-              Login</button>
-        </form>
-
+         />
+        <LoginForm />
+        
         <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <Button asChild>
           <Link
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
+            href="/"
+          >
+            Move to Last Page
+          </Link>
+          </Button>
+
+          <Button asChild>
+          <Link
             href="/studentLogin"
-            rel="noopener noreferrer"
           >
-            Student Home-Page
+            Move to Next Page
           </Link>
-          <Link
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="/createAccount"
-            rel="noopener noreferrer"
-          >
-            Sign-Up
-          </Link>
+          </Button>
         </div>
       </main>
     </div>
