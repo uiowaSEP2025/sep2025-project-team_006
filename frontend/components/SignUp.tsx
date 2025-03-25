8f91f3581f022cdf0d1ae9e1d1fef1520cfdc9c8
joBrady <brady.joe63@gmail.com>
2025-03-25 17:46:36 -0500

    Properly labeld the Sign-Up Component

--- frontend/components/SignUp.tsx
+++ frontend/components/SignUp.tsx
@@ -18,7 +18,7 @@ export function SignUpForm({
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader>
-          <CardTitle className="text-2xl">Login</CardTitle>
+          <CardTitle className="text-2xl">Sign-Up</CardTitle>
           <CardDescription>
             Enter your email below to Sign-Up for your account
           </CardDescription>
