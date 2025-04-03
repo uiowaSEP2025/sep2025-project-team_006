0bd61665a5a6d6a2473189c4e9f5bc180edbd700
Brandon Cano <brandoncano01@gmail.com>
2025-04-02 19:21:42 -0500

    Update tooltips.spec.tsx

--- frontend/__tests__/components/tooltips.spec.tsx
+++ frontend/__tests__/components/tooltips.spec.tsx
@@ -1,4 +1,3 @@
-// __tests__/components/tooltips.spec.tsx
 import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
