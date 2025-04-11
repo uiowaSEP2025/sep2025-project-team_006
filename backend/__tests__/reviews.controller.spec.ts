8e1ca118d7945549cab438079e76ed88c18f03fd
Brandon Cano <brandoncano01@gmail.com>
2025-04-10 21:52:28 -0500

    fixed unit tests

--- backend/__tests__/reviews.controller.spec.ts
+++ backend/__tests__/reviews.controller.spec.ts
@@ -1,15 +1,20 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ReviewsController } from 'src/modules/reviews/reviews.controller';
 import { ReviewsService } from 'src/modules/reviews/reviews.service';
+import { Departments } from 'src/modules/templates/departments.enum';
 
 describe('ReviewsController', () => {
     let controller: ReviewsController;
     let service: ReviewsService;
 
-    const createReviewDto = { faculty_id: 1, application_id: 2 };
+    const createReviewDto = { faculty_id: 1, application_id: 8, department: 'ECE' as Departments };
     const review = {
-        review_id: 6,
+        review_id: 1,
         review_metrics: [],
+        comments: '',
+        overall_score: null,
+        submitted: false,
+        template: { template_id: '123', department: 'ECE', name: 'Electrical and Computer Engineering Template' }
     };
 
     beforeEach(async () => {
@@ -20,7 +25,7 @@ describe('ReviewsController', () => {
                     provide: ReviewsService,
                     useValue: {
                         createReview: jest.fn().mockResolvedValue(review),
-                        updateReviewComments: jest.fn(),
+                        updateReview: jest.fn(),
                         submitReview: jest.fn(),
                     },
                 },
@@ -32,28 +37,44 @@ describe('ReviewsController', () => {
     });
 
     describe('createReview', () => {
-        it('should return a review', async () => {
+        it('should return a review with the template and metrics attached', async () => {
             const result = await controller.createReview(createReviewDto);
             expect(result).toEqual(review);
             expect(service.createReview).toHaveBeenCalledWith(createReviewDto);
         });
     });
 
-    describe('updateReviewComments', () => {
-        it('should update review comments and overall_score and return the updated review', async () => {
+    describe('updateReview', () => {
+        it('should update review comments, overall_score and review metrics', async () => {
             const reviewId = 1;
-            const updateDto = { comments: "Updated comments", overall_score: 95 };
-            const updatedReview = { review_id: reviewId, comments: "Updated comments", overall_score: 95, review_metrics: [] };
-            (service.updateReviewComments as jest.Mock).mockResolvedValue(updatedReview);
+            const updateDto = {
+                comments: "Updated comments",
+                overall_score: 95,
+                review_metrics: [
+                    { review_metric_id: 2, selected_weight: 0.5, value: 4 },
+                    { review_metric_id: 3, selected_weight: 0.3, value: 4 }
+                ]
+            };
+            const updatedReview = {
+                review_id: reviewId,
+                comments: "Updated comments",
+                overall_score: 95,
+                review_metrics: [
+                    { review_metric_id: 2, name: 'Communication', selected_weight: 0.5, template_weight: 0.5, value: 4 },
+                    { review_metric_id: 3, name: 'Expertise', selected_weight: 0.3, template_weight: 0.3, value: 4 }
+                ],
+                submitted: false,
+            };
+            (service.updateReview as jest.Mock).mockResolvedValue(updatedReview);
 
-            const result = await controller.updateReviewComments(reviewId, updateDto);
+            const result = await controller.updateReview(reviewId, updateDto);
             expect(result).toEqual(updatedReview);
-            expect(service.updateReviewComments).toHaveBeenCalledWith(reviewId, updateDto);
+            expect(service.updateReview).toHaveBeenCalledWith(reviewId, updateDto);
         });
     });
 
     describe('submitReview', () => {
-        it('should mark review as submitted and return the updated review', async () => {
+        it('should mark review as submitted and return updated review', async () => {
             const reviewId = 1;
             const submittedReview = { review_id: reviewId, submitted: true, review_metrics: [] };
             (service.submitReview as jest.Mock).mockResolvedValue(submittedReview);
