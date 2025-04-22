3cea6c6a2beaae7a2e2ebac191b67fd491b4e13b
Brandon Cano <brandoncano01@gmail.com>
2025-04-21 21:29:28 -0500

    added studentHome tests

--- frontend/app/(student)/students/page.tsx
+++ frontend/app/(student)/students/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 import { LoginForm } from "@/components/Login";
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+export default function StudentLogin() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
