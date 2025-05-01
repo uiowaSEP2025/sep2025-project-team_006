d34b03ec01934469e7d09fa35fdc25be1157bea9
mechabubba <stevenvanniisprettycool@gmail.com>
2025-05-01 14:08:46 -0500

    add dtos

---
+++ backend/src/modules/applications/applications.enum.ts
@@ -0,0 +1,14 @@
+export enum ApplicationDepartmentType {
+    BME = "BME",
+    CBE = "CBE",
+    CEE = "CEE",
+    ECE = "ECE",
+    ISE = "ISE",
+    ME  = "ME",
+};
+
+export enum ApplicationDegreeProgramType {
+    MS = "MS",
+    MSMT = "MSMT",
+    PHD = "PHD"
+};
